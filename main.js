971ebd0043902667c1f8abcba175d952cf7fb104
Stefan-ci <75357402+Stefan-ci@users.noreply.github.com>
2021-04-02 04:22:01 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+//console.log('Hello World!');
+
+swal({
+  title: "Hey you!",
+  text: "Upvote please,follow‚ like and share! Make suggestions for more better GUI Apps!\nThank you 🤝",
+  icon: "success",
+  button: "Okay",
+});
